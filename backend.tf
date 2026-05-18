3703033a37e2caad9a6540b519b12d7541d2c868
aumthakkar <aumthakkar@yahoo.co.uk>
2026-05-18 17:19:46 +0100

    modified outputs.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    region = "eu-north-1"
-    key    = "eks-cluster/terraform.tfstate"
-    bucket = "pht-dev-eks-cluster-state"
-  }
-
-}
--- outputs.tf
+++ outputs.tf
@@ -7,5 +7,5 @@ output "cluster_endpoint" {
 }
 
 output "cluster_ca_certificate" {
-  value = base64decode(aws_eks_cluster.my_eks_cluster.certificate_authority[0].data)
+  value = aws_eks_cluster.my_eks_cluster.certificate_authority[0].data
 }
